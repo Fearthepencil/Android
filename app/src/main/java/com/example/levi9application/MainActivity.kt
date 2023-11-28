8b7edacbd0c103937d5b003ca176c1581ed71138
pavle stefanovic <pavles.dev@gmail.com>
2023-11-28 10:25:36 +0100

    Fixed duration

--- app/src/main/java/com/example/levi9application/MainActivity.kt
+++ app/src/main/java/com/example/levi9application/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.animation.AnticipateInterpolator
 import android.view.animation.Interpolator
@@ -13,10 +14,13 @@ import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    private var keepSplashOnScreen = true
+    private val delay = 2000L
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
         setContentView(R.layout.activity_main)
 
     }
