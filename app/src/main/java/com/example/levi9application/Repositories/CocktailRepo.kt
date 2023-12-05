74cd57d28fa6298430e3288771660d0143176ad9
pavle stefanovic <pavles.dev@gmail.com>
2023-12-05 12:12:25 +0100

    Add Interface

--- app/src/main/java/com/example/levi9application/Repositories/CocktailRepo.kt
+++ app/src/main/java/com/example/levi9application/Repositories/CocktailRepo.kt
@@ -3,9 +3,6 @@ package com.example.levi9application.repositories
 import com.example.levi9application.network.APIService
 import javax.inject.Inject
 
-class CocktailRepo
-@Inject
-    constructor(private val apiService: APIService) {
-    suspend fun getCocktailList() = apiService.getCocktailList()
-
+class CocktailRepo @Inject constructor(private val apiService: APIService) : CocktailRepoInterface {
+   override suspend fun getCocktailList() = apiService.getCocktailList()
 }
