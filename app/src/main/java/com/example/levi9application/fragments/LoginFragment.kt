4b585aecf917e83643225db1ba900cb4102b521a
pavle stefanovic <pavles.dev@gmail.com>
2023-12-19 14:35:09 +0100

    Add AuthActivity

---
+++ app/src/main/java/com/example/levi9application/fragments/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.levi9application.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.levi9application.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
